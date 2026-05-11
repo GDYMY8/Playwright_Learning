e5ec22670fc31885e555e6a0b6a0b4c7c5d0cce6
GDYMY8 <80619063+GDYMY8@users.noreply.github.com>
2026-05-11 14:36:11 +0930

    Fix comment formatting in playwright.config.ts
    Updated comment formatting for baseURL setting.

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    Base URL to use in actions like `await page.goto('')`.
+    /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://the-internet.herokuapp.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
