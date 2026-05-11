6bdc1a634ab83667d568530cb01fe0c0340a3688
GDYMY8 <80619063+GDYMY8@users.noreply.github.com>
2026-05-11 11:50:56 +0930

    Delete tests/jd.spec.ts

--- tests/jd.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('example.com page loads', async ({ page }) => {
-  await page.goto('https://example.com');
-  await expect(page).toHaveTitle(/Example/);
-});
