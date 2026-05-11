5e721213eacc3c897f8d4604cb807d9996f6c9d9
GDYMY8 <80619063+GDYMY8@users.noreply.github.com>
2026-05-11 14:08:46 +0930

    Delete tests/herokupp_smoke.spec.ts

--- tests/herokupp_smoke.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Home page loads', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com');
-  await expect(page).toHaveTitle('The Internet');
-});
