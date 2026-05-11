b1360ca2775c23ebc13ab1c61ba657b084497d6d
GDYMY8 <80619063+GDYMY8@users.noreply.github.com>
2026-05-11 15:44:49 +0930

    Add iPad Safari and uncomment mobile browser tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -48,27 +48,34 @@ export default defineConfig({
     name: 'webkit',
     use: { ...devices['Desktop Safari'] },
   },
-
+  
+  {
+      name: 'iPad Safari',
+      use: {
+        browserName: 'webkit',
+        ...devices['iPad (gen 7)'],
+      },
+  },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+     {
+       name: 'Mobile Chrome',
+       use: { ...devices['Pixel 5'] },
+     },
+    {
+       name: 'Mobile Safari',
+       use: { ...devices['iPhone 12'] },
+     },
 
     /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+     {
+       name: 'Microsoft Edge',
+       use: { ...devices['Desktop Edge'], channel: 'msedge' },
+     },
+     {
+       name: 'Google Chrome',
+       use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+     },
   ],
 
   /* Run your local dev server before starting the tests */
