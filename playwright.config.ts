56b7020382dc5f84b5bc3f8642866688bed94c77
GDYMY8 <80619063+GDYMY8@users.noreply.github.com>
2026-05-11 14:26:37 +0930

    Update baseURL configuration in playwright.config.ts
    Removed the initial baseURL setting and updated the shared use settings with a new baseURL.

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,9 +12,6 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({  
-  use: {
-    baseURL: 'https://the-internet.herokuapp.com',
-  },
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -28,8 +25,8 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    Base URL to use in actions like `await page.goto('')`.
+    baseURL: 'https://the-internet.herokuapp.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     screenshot: 'only-on-failure',
